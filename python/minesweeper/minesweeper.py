5756412d824b9a1a50718b03430243c406598748
Alice Dee <alice.dee@guardian.co.uk>
2018-09-30 18:27:33 +0100

    minesweeper python exercism

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -1,4 +1,43 @@
+# fn designed to take a board and return the mine hints
 def board(input_board_array):
-    # Function body starts here
+    if input_board_array == []:
+        return []
+    # mapping each item in board array to its length
+    # resulting length of that set (if all same) is 1 or 0 if no items
     if len(set(map(len, input_board_array))) not in (0,1):
         raise ValueError('not all board rows have same length!')
+    # length of first item in list (ie first row length)
+    numcols_rowlen = len(input_board_array[0])
+    # number of rows
+    numrows_collen = len(input_board_array)
+    # makes input into nested list
+    nested_input = [list(i) for i in input_board_array]
+    # count the mines in a range from just before to just after current i in list
+    # for i in range rowlen: mines = list_input[current_row][i-1:i+2]
+    # nb the above will fail when i is out of range. (ie over row length) 
+    # low_bound = max(0, current_cell -1): high_bound doesn't matter??
+    for current_row in range(numrows_collen):
+        for current_cell in range(numcols_rowlen):
+            # original input must be nil, space or *
+            if nested_input[current_row][current_cell] not in ('',' ', '*'):
+                raise ValueError('invalid board')
+            # if statement says only do the else if its ' '
+            if nested_input[current_row][current_cell] != ' ':
+                continue
+            else:
+                lower_bound = max(current_cell -1 ,0)
+                # counts mines on same row
+                mines = nested_input[current_row][lower_bound:current_cell+2].count('*')
+
+                if current_row > 0:
+                    mines += nested_input[current_row -1 ][lower_bound:current_cell+2].count('*')
+                if current_row < numrows_collen -1:
+                    mines += nested_input[current_row + 1 ][lower_bound:current_cell+2].count('*')
+
+                # don't write in 0s or overwrite mines
+                if mines != 0 and current_cell != '*':
+                    nested_input[current_row][current_cell] = str(mines)
+                else:
+                    continue
+    # un-nests the list
+    return ["".join(lst) for lst in nested_input]
--- python/minesweeper/minesweeper_test.py
+++ python/minesweeper/minesweeper_test.py
@@ -15,125 +15,125 @@ from minesweeper import board
 
 class MinesweeperTest(unittest.TestCase):
 
-    # def test_no_rows(self):
-    #     self.assertEqual(board([]), [])
+    def test_no_rows(self):
+        self.assertEqual(board([]), [])
 
-    # def test_no_columns(self):
-    #     self.assertEqual(board([""]), [""])
+    def test_no_columns(self):
+        self.assertEqual(board([""]), [""])
 
-    # def test_no_mines(self):
-    #     inp = ["   ",
-    #            "   ",
-    #            "   "]
-    #     out = ["   ",
-    #            "   ",
-    #            "   "]
-    #     self.assertEqual(board(inp), out)
+    def test_no_mines(self):
+        inp = ["   ",
+               "   ",
+               "   "]
+        out = ["   ",
+               "   ",
+               "   "]
+        self.assertEqual(board(inp), out)
 
-    # def test_board_with_only_mines(self):
-    #     inp = ["***",
-    #            "***",
-    #            "***"]
-    #     out = ["***",
-    #            "***",
-    #            "***"]
-    #     self.assertEqual(board(inp), out)
+    def test_board_with_only_mines(self):
+        inp = ["***",
+               "***",
+               "***"]
+        out = ["***",
+               "***",
+               "***"]
+        self.assertEqual(board(inp), out)
 
-    # def test_mine_surrounded_by_spaces(self):
-    #     inp = ["   ",
-    #            " * ",
-    #            "   "]
-    #     out = ["111",
-    #            "1*1",
-    #            "111"]
-    #     self.assertEqual(board(inp), out)
+    def test_mine_surrounded_by_spaces(self):
+        inp = ["   ",
+               " * ",
+               "   "]
+        out = ["111",
+               "1*1",
+               "111"]
+        self.assertEqual(board(inp), out)
 
-    # def test_space_surrounded_by_mines(self):
-    #     inp = ["***",
-    #            "* *",
-    #            "***"]
-    #     out = ["***",
-    #            "*8*",
-    #            "***"]
-    #     self.assertEqual(board(inp), out)
+    def test_space_surrounded_by_mines(self):
+        inp = ["***",
+               "* *",
+               "***"]
+        out = ["***",
+               "*8*",
+               "***"]
+        self.assertEqual(board(inp), out)
 
-    # def test_horizontal_line(self):
-    #     inp = [" * * "]
-    #     out = ["1*2*1"]
-    #     self.assertEqual(board(inp), out)
+    def test_horizontal_line(self):
+        inp = [" * * "]
+        out = ["1*2*1"]
+        self.assertEqual(board(inp), out)
 
-    # def test_horizontal_line_mines_at_edges(self):
-    #     inp = ["*   *"]
-    #     out = ["*1 1*"]
-    #     self.assertEqual(board(inp), out)
+    def test_horizontal_line_mines_at_edges(self):
+        inp = ["*   *"]
+        out = ["*1 1*"]
+        self.assertEqual(board(inp), out)
 
-    # def test_vertical_line(self):
-    #     inp = [" ",
-    #            "*",
-    #            " ",
-    #            "*",
-    #            " "]
-    #     out = ["1",
-    #            "*",
-    #            "2",
-    #            "*",
-    #            "1"]
-    #     self.assertEqual(board(inp), out)
+    def test_vertical_line(self):
+        inp = [" ",
+               "*",
+               " ",
+               "*",
+               " "]
+        out = ["1",
+               "*",
+               "2",
+               "*",
+               "1"]
+        self.assertEqual(board(inp), out)
 
-    # def test_vertical_line_mines_at_edges(self):
-    #     inp = ["*",
-    #            " ",
-    #            " ",
-    #            " ",
-    #            "*"]
-    #     out = ["*",
-    #            "1",
-    #            " ",
-    #            "1",
-    #            "*"]
-    #     self.assertEqual(board(inp), out)
+    def test_vertical_line_mines_at_edges(self):
+        inp = ["*",
+               " ",
+               " ",
+               " ",
+               "*"]
+        out = ["*",
+               "1",
+               " ",
+               "1",
+               "*"]
+        self.assertEqual(board(inp), out)
 
-    # def test_cross(self):
-    #     inp = ["  *  ",
-    #            "  *  ",
-    #            "*****",
-    #            "  *  ",
-    #            "  *  "]
-    #     out = [" 2*2 ",
-    #            "25*52",
-    #            "*****",
-    #            "25*52",
-    #            " 2*2 "]
-    #     self.assertEqual(board(inp), out)
+    def test_cross(self):
+        inp = ["  *  ",
+               "  *  ",
+               "*****",
+               "  *  ",
+               "  *  "]
+        out = [" 2*2 ",
+               "25*52",
+               "*****",
+               "25*52",
+               " 2*2 "]
+        self.assertEqual(board(inp), out)
 
-    # def test_large_board(self):
-    #     inp = [" *  * ",
-    #            "  *   ",
-    #            "    * ",
-    #            "   * *",
-    #            " *  * ",
-    #            "      "]
-    #     out = ["1*22*1",
-    #            "12*322",
-    #            " 123*2",
-    #            "112*4*",
-    #            "1*22*2",
-    #            "111111"]
-    #     self.assertEqual(board(inp), out)
+    def test_large_board(self):
+        inp = [" *  * ",
+               "  *   ",
+               "    * ",
+               "   * *",
+               " *  * ",
+               "      "]
+        out = ["1*22*1",
+               "12*322",
+               " 123*2",
+               "112*4*",
+               "1*22*2",
+               "111111"]
+        self.assertEqual(board(inp), out)
 
-    # # Additional test for this track
-    # def test_board9(self):
-    #     inp = ["     ",
-    #            "   * ",
-    #            "     ",
-    #            "     ",
-    #            " *   "]
-    #     out = ["  111",
-    #            "  1*1",
-    #            "  111",
-    #            "111  ",
-    #            "1*1  "]
-    #     self.assertEqual(board(inp), out)
+    # Additional test for this track
+    def test_board9(self):
+        inp = ["     ",
+               "   * ",
+               "     ",
+               "     ",
+               " *   "]
+        out = ["  111",
+               "  1*1",
+               "  111",
+               "111  ",
+               "1*1  "]
+        self.assertEqual(board(inp), out)
 
     def test_different_len(self):
         inp = [" ",
@@ -142,17 +142,17 @@ class MinesweeperTest(unittest.TestCase):
         with self.assertRaisesWithMessage(ValueError):
             board(inp)
 
-    # def test_invalid_char(self):
-    #     inp = ["X  * "]
-    #     with self.assertRaisesWithMessage(ValueError):
-    #         board(inp)
+    def test_invalid_char(self):
+        inp = ["X  * "]
+        with self.assertRaisesWithMessage(ValueError):
+            board(inp)
 
-    # # Utility functions
-    # def setUp(self):
-    #     try:
-    #         self.assertRaisesRegex
-    #     except AttributeError:
-    #         self.assertRaisesRegex = self.assertRaisesRegexp
+    # Utility functions
+    def setUp(self):
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
 
     def assertRaisesWithMessage(self, exception):
         return self.assertRaisesRegex(exception, r".+")
